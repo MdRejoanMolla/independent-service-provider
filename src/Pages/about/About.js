c1a1251154eb48005c635af9f1aa10f797671e6d
MdRejoanMolla <mollamdrejoan@gmail.com>
2022-04-18 16:18:14 +0600

    about done

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
       return (
-            <div>
-                  <h2>about</h2>
+            <div className='about'>
+                  <h2>Md Rejoan Molla</h2>
+                  <p>I become a good developer.So, I try to work hard. I improve my skills and capabilities. I make my times good.  </p>
             </div>
       );
 };
